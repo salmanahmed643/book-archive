2db0e99110e520821d2126cfd35fcb96659241ae
salmanahmed643 <647salman@gmail.com>
2021-09-02 19:50:09 +0600

    updated

--- js/bookarchive.js
+++ js/bookarchive.js
@@ -5,19 +5,18 @@ const SearchBook = () => {
     searchField.value = '';
 
     // fetch
-    const url = `http://openlibrary.org/search.json?q=${searchText}`
+    const url = `https://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
     .then(res => res.json())
     .then(data => displaySearchBooks(data.docs))
 }
 
 const displaySearchBooks = books => {
-    // console.log(books)
     const searchContainer = document.getElementById('search_container');
     searchContainer.textContent = '';
+
     // for each
     books.forEach(book => {
-        console.log(book)
         const div = document.createElement('div');
         const imgUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`;
         div.classList.add('div');
@@ -29,48 +28,22 @@ const displaySearchBooks = books => {
 
                 <div class="card-body">
                     <h5 class="card-title">
-                        <span class="fw-bolder">Name:</span> ${book.title}
+                        <span class="fw-bolder mb-5">Name:</span> ${book.title}
                     </h5>
                     <p class="card-text">
-                        <span class="fw-bolder">Author:</span> ${book.author_name}
+                        <span class="fw-bolder">Author:</span> <span class="fst-italic"> ${book.author_name} </span>
                         <br>
                         <span class="fw-bolder">Publisher:</span> ${book.publisher}
                         <br>
-                        <span class="fw-bolder">Publish:</span> ${book.first_publish_year}
+                        <span class="fw-bolder">Publish:</span> <span class="text-muted">${book.first_publish_year}</span>
                     </p>
                 </div>
 
                 <div class="card-footer text-center">
-                    <button onclick="loadBook('${book.author_key}')" class="btn btn-transform border border-1 border-dark fw-bold"> More Info </button>
+                    <button class="btn btn-transform fw-bold"> Buy Now </button>
                 </div>
             </div>
         `;
         searchContainer.appendChild(div);
     })
-} 
-
-
-// working
-
-const loadBook = authorKey => {
-    const url = `https://openlibrary.org/authors/${authorKey}.json`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => singleBookDetail(data))
-}
-
-const singleBookDetail = book => {
-    console.log(book);
-    const singleContainer = document.getElementById('single_container');
-    
-    singleContainer.innerHTML = `
-        <div class="card-header bg-transparent border-success"> ${book.name} </div>
-        <div class="card-body text-success">
-        
-              <h5 class="card-title">Success card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        <div class="card-footer bg-transparent border-success">Footer</div>
-    `;
-    window.scroll(0, 40)
 }
